65d055d0946a1659e7fd2d2c3a5012080ed68855
Denzel Stellingwerf <d.stellingwerf@student.tudelft.nl>
2025-01-24 18:00:38 +0100

    feat: remove expo from docker compose

--- api/index.ts
+++ api/index.ts
@@ -14,36 +14,9 @@ const db = drizzle(process.env.DATABASE_URL!);
 // // Main function test db connection
 async function main() {
   console.log('Connecting to the database...')
-  const user: typeof usersTable.$inferInsert = {
-    username: 'John',
-    email: 'john@example.com',
-  };
-    await db.insert(usersTable).values(user);
-
-  await db.insert(usersTable).values(user);
-  console.log('New user created!')
 
   const users = await db.select().from(usersTable);
   console.log('Getting all users from the database: ', users)
-  /*
-  const users: {
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-  }[]
-  */
-
-  await db
-    .update(usersTable)
-    .set({
-      username: "ligma",
-    })
-    .where(eq(usersTable.email, user.email));
-  console.log('User info updated!')
-
-  await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  console.log('User deleted!')
 }
 
 main();
