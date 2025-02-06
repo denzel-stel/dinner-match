3148abfed693b35811f258326701446e095ccd2e
Denzel Stellingwerf <d.stellingwerf@student.tudelft.nl>
2025-02-06 08:33:32 +0100

    feat: include api repo as module

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Main entry point for the API
- * This file imports all the other files in the api directory.
- */
-
-import "./api";
-import "./routes/index";
-import "../database/seeders/index";
